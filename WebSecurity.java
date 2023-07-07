80e59e26dcd047513affd1bc9d9584447ad29206
ShanmugananthamS <138778235+ShanmugananthamS@users.noreply.github.com>
2023-07-07 12:37:41 +0530

    Add files via upload
    Added

---
+++ WebSecurity.java
@@ -0,0 +1,51 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurity {
+
+	@Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder){
+		
+		UserDetails staff = User.withUsername("Shweta")
+					.password(passwordEncoder.encode("shweta"))
+				.roles("STAFF").build();
+		
+		UserDetails admin = User.withUsername("Riya")
+				.password(passwordEncoder.encode("riya"))
+			.roles("ADMIN").build();
+		
+		
+		return new InMemoryUserDetailsManager(staff,admin);
+		
+		
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+				.antMatchers("/addProfile").hasRole("STAFF")
+				.antMatchers("/updateProfile").hasAnyRole("ADMIN","STAFF")
+				.anyRequest().authenticated().and().httpBasic();
+		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		return encoder;
+
+	}
+
+}
